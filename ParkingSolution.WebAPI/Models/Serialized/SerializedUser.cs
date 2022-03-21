b8c6feb088f68fddf1763be84c3725c9f9cf1eca
thepicture <85333076+thepicture@users.noreply.github.com>
2022-03-21 16:49:12 +0000

    feat(userscontroller): implement the controller

---
+++ ParkingSolution.WebAPI/Models/Serialized/SerializedUser.cs
@@ -0,0 +1,24 @@
+﻿using ParkingSolution.WebAPI.Models.Entities;
+
+namespace ParkingSolution.WebAPI.Models.Serialized
+{
+    public class SerializedUser
+    {
+        public SerializedUser()
+        {
+        }
+
+        public SerializedUser(User user)
+        {
+            Id = user.Id;
+            PhoneNumber = user.PhoneNumber;
+            UserTypeId = user.UserTypeId;
+            UserType = user.UserType.Name;
+        }
+        public int Id { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public int UserTypeId { get; set; }
+        public string UserType { get; set; }
+    }
+}
