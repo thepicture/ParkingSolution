c3f8a54abdaddb836509c8704f3b979882988cfa
thepicture <85333076+thepicture@users.noreply.github.com>
2022-03-26 22:08:23 +0500

    improve(parkings): remove map showing in the list mode

---
+++ ParkingSolution.XamarinApp/ParkingSolution.XamarinApp/Controls/BindableToolbarItem.cs
@@ -0,0 +1,59 @@
+﻿using Xamarin.Forms;
+
+namespace ParkingSolution.XamarinApp.Controls
+{
+    public class BindableToolbarItem : ToolbarItem
+    {
+        public static readonly BindableProperty IsVisibleProperty =
+            BindableProperty
+            .Create(
+                "IsVisible",
+                typeof(bool),
+                typeof(BindableToolbarItem),
+                true,
+                BindingMode.TwoWay,
+                propertyChanged: OnIsVisibleChanged);
+
+        protected override void OnParentSet()
+        {
+            base.OnParentSet();
+            InitVisibility();
+        }
+
+        public bool IsVisible
+        {
+            get { return (bool)GetValue(IsVisibleProperty); }
+            set { SetValue(IsVisibleProperty, value); }
+        }
+
+        private void InitVisibility()
+        {
+            OnIsVisibleChanged(this, false, IsVisible);
+        }
+
+        private static void OnIsVisibleChanged(
+            BindableObject bindable,
+            object oldvalue,
+            object newvalue)
+        {
+            var item = bindable as BindableToolbarItem;
+
+            if (item != null && item.Parent == null)
+                return;
+
+            if (item != null)
+            {
+                var items = ((ContentPage)item.Parent).ToolbarItems;
+
+                if ((bool)newvalue && !items.Contains(item))
+                {
+                    Device.BeginInvokeOnMainThread(() => { items.Add(item); });
+                }
+                else if (!(bool)newvalue && items.Contains(item))
+                {
+                    Device.BeginInvokeOnMainThread(() => { items.Remove(item); });
+                }
+            }
+        }
+    }
+}
